7669226c5118709c89e14cb2470423eb62cc23e5
Fernando Velarde <f.velarde@hotmail.com>
2019-11-18 15:03:04 -0600

    In-class day 1

--- Movies/MovieDatabase.cs
+++ Movies/MovieDatabase.cs
@@ -17,8 +17,9 @@ namespace Movies
         /// <summary>
         /// Loads the movie database from the JSON file
         /// </summary>
-        public MovieDatabase() {
-            
+        public MovieDatabase()
+        {
+
             using (StreamReader file = System.IO.File.OpenText("movies.json"))
             {
                 string json = file.ReadToEnd();
@@ -27,5 +28,45 @@ namespace Movies
         }
 
         public List<Movie> All { get { return movies; } }
+
+        public List<Movie> SearchAndFilter(string searchString, List<string> rating)
+        {
+            //case 0: no search string, no ratings
+            if (searchString == null && rating.Count == 0) return All;
+
+            List<Movie> results = new List<Movie>();
+            foreach (Movie movie in movies)
+            {
+                //case 1: search string and ratings
+                if (searchString != null && rating.Count > 0)
+                {
+                    if(movie.Title != null 
+                        && movie.Title.Contains(searchString, StringComparison.InvariantCultureIgnoreCase)
+                        && rating.Contains(movie.MPAA_Rating)){
+                        results.Add(movie);
+                    }
+                }
+                //case 2: search string only
+                else if (searchString != null)
+                {
+                    if (movie.Title != null && movie.Title.Contains(searchString, StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        results.Add(movie);
+                    }
+                }
+
+
+                //case 3: search ratings only
+                else if (rating.Count > 0)
+                {
+
+                    if (rating.Contains(movie.MPAA_Rating))
+                    {
+                        results.Add(movie);
+                    }
+                }
+            }
+            return results;
+        }
     }
 }
