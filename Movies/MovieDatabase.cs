c265a38a38c79aa5ab2a19185805322c82a0f27a
fvelarde <fvelarde@F1092-19-2107>
2019-11-20 14:57:43 -0600

    Class day2

--- Movies/MovieDatabase.cs
+++ Movies/MovieDatabase.cs
@@ -10,60 +10,72 @@ namespace Movies
     /// <summary>
     /// A class representing a database of movies
     /// </summary>
-    public class MovieDatabase
+    public static class MovieDatabase //static means you cant make an instance of it, all behavior
     {
-        private List<Movie> movies = new List<Movie>();
-
-        /// <summary>
-        /// Loads the movie database from the JSON file
-        /// </summary>
-        public MovieDatabase()
+        private static List<Movie> movies;
+        public static List<Movie> All
         {
-
-            using (StreamReader file = System.IO.File.OpenText("movies.json"))
+            get
             {
-                string json = file.ReadToEnd();
-                movies = JsonConvert.DeserializeObject<List<Movie>>(json);
+                if (movies == null)
+                {
+                    using (StreamReader file = System.IO.File.OpenText("movies.json"))
+                    {
+                        string json = file.ReadToEnd();
+                        movies = JsonConvert.DeserializeObject<List<Movie>>(json);
+                    }
+                }
+                return movies;
             }
         }
-
-        public List<Movie> All { get { return movies; } }
-
-        public List<Movie> SearchAndFilter(string searchString, List<string> rating)
+        public static List<Movie> Search(List<Movie> movies, string term)
         {
-            //case 0: no search string, no ratings
-            if (searchString == null && rating.Count == 0) return All;
-
             List<Movie> results = new List<Movie>();
+
             foreach (Movie movie in movies)
             {
-                //case 1: search string and ratings
-                if (searchString != null && rating.Count > 0)
+                if (movie.Title != null && movie.Title.Contains(term, StringComparison.OrdinalIgnoreCase))
                 {
-                    if(movie.Title != null 
-                        && movie.Title.Contains(searchString, StringComparison.InvariantCultureIgnoreCase)
-                        && rating.Contains(movie.MPAA_Rating)){
-                        results.Add(movie);
-                    }
+                    results.Add(movie);
                 }
-                //case 2: search string only
-                else if (searchString != null)
+            }
+            return results;
+        }
+        public static List<Movie> FilterByMPAA(List<Movie> movies, List<string> mpaa)
+        {
+            List<Movie> results = new List<Movie>();
+
+            foreach (Movie movie in movies)
+            {
+                if (mpaa.Contains(movie.MPAA_Rating)) ;
                 {
-                    if (movie.Title != null && movie.Title.Contains(searchString, StringComparison.InvariantCultureIgnoreCase))
-                    {
-                        results.Add(movie);
-                    }
+                    results.Add(movie);
                 }
+            }
+            return results;
+        }
+        public static List<Movie> FilterByMinIMDB(List<Movie> movies, float minIMDB)
+        {
+            List<Movie> results = new List<Movie>();
 
-
-                //case 3: search ratings only
-                else if (rating.Count > 0)
+            foreach (Movie movie in movies)
+            {
+                if (movie.IMDB_Rating != null && minIMDB <= movie.IMDB_Rating)
                 {
+                    results.Add(movie);
+                }
+            }
+            return results;
+        }
+        public static List<Movie> FilterByMaxIMDB(List<Movie> movies, float maxIMDB)
+        {
+            List<Movie> results = new List<Movie>();
 
-                    if (rating.Contains(movie.MPAA_Rating))
-                    {
-                        results.Add(movie);
-                    }
+            foreach (Movie movie in movies)
+            {
+                if (movie.IMDB_Rating != null && maxIMDB >= movie.IMDB_Rating)
+                {
+                    results.Add(movie);
                 }
             }
             return results;
